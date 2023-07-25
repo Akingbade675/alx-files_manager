e44faa805bc43da0847e1822a3f2083fc9c0e470
Akingbade675 <akingbadebasit26@gmail.com>
2023-07-25 11:49:31 +0000

    added Task.md

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-import express from 'express'
-import routes from './routes'
+import express from 'express';
+import indexRoute from './routes/index';
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const PORT = 5000;
 
-app.use(express.json())
+app.use(express.json({ limit: '50mb' })).use(indexRoute);
 
-app.use('/', routes)
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- worker.js
+++ worker.js
@@ -1,36 +1,38 @@
-import Queue from 'bull'
-import imageThumbnail from 'image-thumbnail'
-import { writeFile } from 'fs/promises'
-import filesCollection from './utils/files'
+import Queue from 'bull';
+import imageThumbnail from 'image-thumbnail';
+import { writeFile } from 'fs/promises';
+import filesCollection from './utils/files';
 
-export const fileQueue = new Queue(
+const fileQueue = new Queue(
   'thumbnails generation',
-  'redis://localhost:6379'
-)
+  'redis://localhost:6379',
+);
 
-const THUMBNAIL_WIDTH = [500, 250, 100]
+const THUMBNAIL_WIDTH = [500, 250, 100];
 
 async function generateAndSaveThumbnail(path, width) {
   const thumbnail = await imageThumbnail(path, {
     width,
     responseType: 'base64',
-  })
-  const thumbnailName = `${path}_${width}`
-  await writeFile(thumbnailName, Buffer.from(thumbnail, 'base64'))
+  });
+  const thumbnailName = `${path}_${width}`;
+  await writeFile(thumbnailName, Buffer.from(thumbnail, 'base64'));
 }
 
 fileQueue.process(async (job, done) => {
-  const { fileId, userId } = job.data
+  const { fileId, userId } = job.data;
 
-  if (!fileId) done(new Error('Missing fileId'))
-  if (!userId) done(new Error('Missing userId'))
+  if (!fileId) done(new Error('Missing fileId'));
+  if (!userId) done(new Error('Missing userId'));
 
-  const file = await filesCollection.findUserFileById(userId, fileId)
-  if (!file) return done(new Error('File not found'))
+  const file = await filesCollection.findUserFileById(userId, fileId);
+  if (!file) return done(new Error('File not found'));
 
   THUMBNAIL_WIDTH.forEach(async (width) => {
-    await generateAndSaveThumbnail(file.localPath, width)
-  })
+    await generateAndSaveThumbnail(file.localPath, width);
+  });
 
-  done()
-})
+  done();
+});
+
+export default fileQueue;
